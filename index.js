663d53083018a6ca74bb6d532228b55a89ea1f61
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-15 19:32:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function theBeatlesPlay(musicians, instruments) {
     arr.push(str);
   } return arr;
 }
+
+function johnLennonFacts(facts) {
+  var arr = [];
+  while(arr.length < facts.length) {
+    var count = 0;
+    var str = `${facts[count]} !!! `;
+    arr.push(str);
+  } return arr;
+}
